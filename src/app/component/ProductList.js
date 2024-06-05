50f8d25d1af9e99abdd590b0e9bfb22ce40b870d
ShrutiTechmonk <shruti.moradiya@techmonk.io>
2024-06-05 10:22:56 +0530

    add product review

---
+++ src/app/component/ProductList.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import ProductUrl from "./ProductUrl";
+
+function ProductList({ Product }) {
+  const [showUrls, setShowUrls] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const ProductUrls = () => setShowUrls(!showUrls);
+
+  const product = [
+    { name: "product 1" },
+    { name: "product 2" },
+    { name: "product 3" },
+    { name: "product 4" },
+    { name: "product 5" },
+  ];
+
+  const handleCheckboxChange = (productName) => {
+    setSelectedProducts((prevSelectedProducts) => {
+      if (prevSelectedProducts.includes(productName)) {
+        return prevSelectedProducts.filter((name) => name !== productName);
+      } else {
+        return [...prevSelectedProducts, productName];
+      }
+    });
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProducts = product.filter((p) =>
+    p.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <motion.section
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        className="font-HelveticaNeueLtd fixed inset-0 z-50 flex justify-center items-center bg-opacity-50"
+      >
+        {showUrls ? (
+          <ProductUrl ProductUrls={ProductUrls} />
+        ) : (
+          <div className="w-[80%] md:w-[60%] lg:w-[45%] bg-[#fff] shadow-md rounded-[37px]">
+            <div
+              className="bg-[#f4f1f1] px-6 py-3 rounded-t-[37px] border-b"
+              onClick={Product}
+            >
+              <h1 className="text-base md:text-xl font-medium">Product List</h1>
+            </div>
+
+            <div className="p-6 border-b">
+              <div className="relative flex items-center gap-3">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="w-full border border-gray-400 rounded-md px-8 py-1 outline-none text-normal text-[#343434]"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+                <span className="absolute left-[1%]">
+                  <Image src="/searchIcon.svg" width={18} height={18} />
+                </span>
+              </div>
+            </div>
+
+            <form>
+              <div className="flex flex-col items-start gap-5 px-4 sm:px-7">
+                <ul className="w-full">
+                  {filteredProducts.length > 0 ? (
+                    filteredProducts.map((product, index) => (
+                      <li
+                        key={product.name}
+                        className="flex items-center gap-2 py-2 sm:py-4 text-[14px] border-b md:text-[16px] lg:text-[18px] font-medium"
+                      >
+                        <span>
+                          <input
+                            type="checkbox"
+                            checked={selectedProducts.includes(product.name)}
+                            onChange={() => handleCheckboxChange(product.name)}
+                          />
+                        </span>
+                        {product.name}
+                      </li>
+                    ))
+                  ) : (
+                    <li className="text-[14px] md:text-[16px] lg:text-[18px] font-medium p-5">
+                      Not Found
+                    </li>
+                  )}
+                </ul>
+              </div>
+              <div className="flex justify-between items-center gap-2 sm:gap-3 p-4 sm:p-5">
+                <span className="text-xs sm:text-base text-center">{selectedProducts.length} Product selected</span>
+                <div className="flex gap-1 sm:gap-3">
+                  <button
+                    onClick={Product}
+                    className="text-[#222] border text-[12px] md:text-[16px] font-normal rounded-md w-[3.5rem] sm:w-[4.5rem] h-[2rem]"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={ProductUrls}
+                    className="text-[#222] border text-[12px] md:text-[16px] font-normal rounded-md w-[3.5rem] sm:w-[4.5rem] h-[2rem]"
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+      </motion.section>
+    </>
+  );
+}
+
+export default ProductList;
---
+++ src/app/component/Review.js
@@ -0,0 +1,90 @@
+"use client";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import ProductList from "./ProductList";
+
+function review() {
+    const [showReview, setShowReview] = useState(false);
+  const Review = () => setShowReview(!showReview);
+
+  const [showProduct, setShowProduct] = useState(false);
+  const Product = () => setShowProduct(!showProduct);
+  return (
+    <>
+     <section className=" flex items-center font-generalsans h-fit w-full py-[2rem] md:pt-[6rem] md:pb-[2rem] px-[1rem] md:px-[3rem] lg:px-[3rem] 2xl:px-[5rem]">
+        <div className="container">
+          <div className="w-[80%] bg-[#F3F5F8] p-5 border border-[#00000024] rounded">
+            <div className="flex justify-between items-center" onClick={Review}>
+              <div>
+                <h1 className="text-[#222] text-[12px] md:text-[15px] lg:text-[24px] font-semibold leading-normal">
+                  Import reviews from another platform
+                </h1>
+                <p className="text-[#00000085] text-[12px] md:text-[15px] lg:text-[18px]">
+                  Reviews must be collected from another review management
+                  platform
+                </p>
+              </div>
+
+              <span className={showReview && "rotate-180"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 18 18"
+                  fill="none"
+                >
+                  <path
+                    d="M13.5 6.75L9 11.25L4.5 6.75"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+            </div>
+            {showReview && (
+              <div className="flex gap-4 mt-10">
+                <button
+                  onClick={Product}
+                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                >
+                  Amazon
+                </button>
+                <button
+                  onClick={Product}
+                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                >
+                  Flipkart
+                </button>
+                <button
+                  onClick={Product}
+                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                >
+                  Myntra
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+
+      <AnimatePresence>
+        {showProduct && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.5 }}
+              exit={{ opacity: 0 }}
+              onClick={Product}
+              className="fixed inset-0 bg-[#001632] z-40"
+            />
+            <ProductList Product={Product} />
+          </>
+        )}
+      </AnimatePresence> 
+    </>
+  )
+}
+
+export default review
