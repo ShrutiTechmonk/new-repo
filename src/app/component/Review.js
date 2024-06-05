5815669cd3c5eba1a2c5e033017744fb98eb532b
ShrutiTechmonk <shruti.moradiya@techmonk.io>
2024-06-05 10:43:43 +0530

    add product review

--- src/app/component/Review.js
+++ src/app/component/Review.js
@@ -13,8 +13,8 @@ function review() {
     <>
      <section className=" flex items-center font-generalsans h-fit w-full py-[2rem] md:pt-[6rem] md:pb-[2rem] px-[1rem] md:px-[3rem] lg:px-[3rem] 2xl:px-[5rem]">
         <div className="container">
-          <div className="w-[80%] bg-[#F3F5F8] p-5 border border-[#00000024] rounded">
-            <div className="flex justify-between items-center" onClick={Review}>
+          <div className="lg:w-[80%] bg-[#F3F5F8] p-5 border border-[#00000024] rounded">
+            <div className="flex justify-between gap-3 items-center" onClick={Review}>
               <div>
                 <h1 className="text-[#222] text-[12px] md:text-[15px] lg:text-[24px] font-semibold leading-normal">
                   Import reviews from another platform
@@ -44,22 +44,22 @@ function review() {
               </span>
             </div>
             {showReview && (
-              <div className="flex gap-4 mt-10">
+              <div className="flex gap-2 sm:gap-4 mt-10">
                 <button
                   onClick={Product}
-                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                  class="text-[#019ed1] text-[13px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
                 >
                   Amazon
                 </button>
                 <button
                   onClick={Product}
-                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                  class="text-[#019ed1] text-[13px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
                 >
                   Flipkart
                 </button>
                 <button
                   onClick={Product}
-                  class="text-[#019ed1] text-[14px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
+                  class="text-[#019ed1] text-[13px] md:text-[16px] leading-[21.6px] rounded-md border border-[#E6E5F6] bg-[#fff] hover:bg-opacity-90 hover:shadow-md transition-all duration-200 ease-in-out transform hover:scale-[1.02] w-[8rem] h-[2.5rem]"
                 >
                   Myntra
                 </button>
